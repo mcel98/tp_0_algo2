dd797392ad1e7b74877ae469f192e1912f9864a4
jampuero <ampuero.jose96@gmail.com>
2018-04-03 19:09:21 -0300

    calculadora

--- src/Calculadora.cpp
+++ src/Calculadora.cpp
@@ -1,4 +1,24 @@
 //
 // Created by jampuero on 03/04/18.
 //
+#include "Calculadora.h"
+Calculadora::Var::Var(Id idVar, int valor) {
+    IdVar = idVar;
+    Valor = valor;
+}
+Calculadora::Memoria::Memoria() {
+    vector<Var> Mem;
+    _Mem = Mem;
+
+}
+Calculadora::Calculadora(Programa programa){
+    _programa = programa;
+
+}
+
+
+void Calculadora::Memoria::asignarVariable(Id idVariable, int valor) {
+    Var variable = Var(idVariable,valor);
+    _Mem.push_back(variable);
+}
 
--- src/Calculadora.h
+++ src/Calculadora.h
@@ -5,4 +5,33 @@
 #ifndef SOLUCION_CALCULADORA_H
 #define SOLUCION_CALCULADORA_H
 
+#include "Programa.h"
+class Calculadora{
+
+public:
+    Calculadora(Programa programa);
+
+
+private:
+    struct Var{
+        Id IdVar;
+        int Valor;
+        Var(Id idVar,int valor);
+
+    };
+    struct Memoria{
+        void asignarVariable(Id idVariable, int valor);
+        Memoria();
+        vector <Var> _Mem;
+
+    };
+    vector<int> pila;
+    Programa _programa;
+
+
+
+
+
+};
+
 #endif //SOLUCION_CALCULADORA_H
